1a56380f9ef3646c6d309d93173dcf33d67ce976
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-07-29 00:00:00 +0200

    Cleaned form.

--- src/Form/CommentForm.php
+++ src/Form/CommentForm.php
@@ -3,12 +3,7 @@ namespace Comment\Form;
 
 use Omeka\Stdlib\Message;
 use Omeka\View\Helper\Setting;
-use Zend\Form\Element\Button;
-use Zend\Form\Element\Checkbox;
-use Zend\Form\Element\Csrf;
-use Zend\Form\Element\Email;
-use Zend\Form\Element\Hidden;
-use Zend\Form\Element\Text;
+use Zend\Form\Element;
 use Zend\Form\Form;
 use Zend\Http\PhpEnvironment\RemoteAddress;
 use Zend\ServiceManager\ServiceLocatorInterface as FormElementManager;
@@ -52,14 +47,15 @@ class CommentForm extends Form
             ? $urlHelper('site/comment', ['action' => 'add', 'site-slug' => $siteSlug])
             : $urlHelper('admin/comment', ['action' => 'add']);
 
-        $this->setAttribute('id', 'comment-form');
-        $this->setAttribute('action', $action);
-        $this->setAttribute('class', 'comment-form disable-unsaved-warning');
-        $this->setAttribute('data-resource-id', $resourceId);
+        $this
+            ->setAttribute('id', 'comment-form')
+            ->setAttribute('action', $action)
+            ->setAttribute('class', 'comment-form disable-unsaved-warning')
+            ->setAttribute('data-resource-id', $resourceId);
 
         if ($isAnonymous) {
             $this->add([
-                'type' => Text::class,
+                'type' => Element\Text::class,
                 'name' => 'o:name',
                 'options' => [
                     'label' => 'Name', // @translate
@@ -72,7 +68,7 @@ class CommentForm extends Form
             ]);
 
             $this->add([
-                'type' => Email::class,
+                'type' => Element\Email::class,
                 'name' => 'o:email',
                 'options' => [
                     'label' => 'Email', // @translate
@@ -85,7 +81,7 @@ class CommentForm extends Form
             ]);
 
             $this->add([
-                'type' => \Zend\Form\Element\Url::class,
+                'type' => Element\Url::class,
                 'name' => 'o-module-comment:website',
                 'options' => [
                     'label' => 'Website', // @translate
@@ -97,7 +93,7 @@ class CommentForm extends Form
         }
 
         $this->add([
-            'type' => 'Textarea',
+            'type' => Element\Textarea::class,
             'name' => 'o-module-comment:body',
             'options' => [
                 'label' => 'Comment', // @translate
@@ -129,7 +125,7 @@ class CommentForm extends Form
                 // TODO Allow html legal agreement in the comment form help from here.
                 $legalText = str_replace('&nbsp;', ' ', strip_tags($legalText));
                 $this->add([
-                    'type' => Checkbox::class,
+                    'type' => Element\Checkbox::class,
                     'name' => 'legal_agreement',
                     'options' => [
                         'label' => 'Terms of service', // @translate
@@ -158,7 +154,7 @@ class CommentForm extends Form
                 $secretKey = $settingHelper('recaptcha_secret_key');
                 if ($siteKey && $secretKey) {
                     $element = $this->getFormElementManager()
-                        ->get('Omeka\Form\Element\Recaptcha', [
+                        ->get(\Omeka\Form\Element\Recaptcha::class, [
                             'site_key' => $siteKey,
                             'secret_key' => $secretKey,
                             'remote_ip' => (new RemoteAddress)->getIpAddress(),
@@ -170,38 +166,40 @@ class CommentForm extends Form
                     // Return only one digit.
                     $a = mt_rand(0, 6);
                     $b = mt_rand(1, 3);
+
                     $result = (string) ($a + $b);
 
                     $question = new Message('How much is %d plus %d?', $a, $b); // @translate
                     // Use the name "address" for spam.
-                    $this->add(['type' => Hidden::class, 'name' => 'address_a', 'attributes' => ['value' => $a]]);
-                    $this->add(['type' => Hidden::class, 'name' => 'address_b', 'attributes' => ['value' => $b]]);
-                    $this->add([
-                        'type' => Text::class,
-                        'name' => 'address',
-                        'options' => [
-                            'label' => (string) new Message('To prove you are not a robot, answer this question: %s', $question), // @translate
-                            'required' => true,
-                        ],
-                        'validators' => [
-                            ['notEmpty', true, [
-                                'messages' => [
-                                    'isEmpty' => 'You must answer the antispam question.', // @translate
-                                ],
-                            ]],
-                            ['identical', false, [
-                                'token' => $result,
-                                'messages' => [
-                                    'notSame' => 'Check your anwser to the antispam question.', // @translate
-                                ],
-                            ]],
-                        ],
-                    ]);
+                    $this
+                        ->add(['type' => Element\Hidden::class, 'name' => 'address_a', 'attributes' => ['value' => $a]])
+                        ->add(['type' => Element\Hidden::class, 'name' => 'address_b', 'attributes' => ['value' => $b]])
+                        ->add([
+                            'type' => Element\Text::class,
+                            'name' => 'address',
+                            'options' => [
+                                'label' => (string) new Message('To prove you are not a robot, answer this question: %s', $question), // @translate
+                                'required' => true,
+                            ],
+                            'validators' => [
+                                ['notEmpty', true, [
+                                    'messages' => [
+                                        'isEmpty' => 'You must answer the antispam question.', // @translate
+                                    ],
+                                ]],
+                                ['identical', false, [
+                                    'token' => $result,
+                                    'messages' => [
+                                        'notSame' => 'Check your anwser to the antispam question.', // @translate
+                                    ],
+                                ]],
+                            ],
+                        ]);
                 }
 
                 // An honeypot for anti-spam. It’s hidden, so only bots fill it.
                 $this->add([
-                    'type' => Text::class,
+                    'type' => Element\Text::class,
                     'name' => 'o-module-comment:check',
                     'options' => [
                         'label' => 'String to check', // @translate
@@ -221,57 +219,52 @@ class CommentForm extends Form
             }
         }
 
-        $this->add([
-            'type' => Hidden::class,
-            'name' => 'resource_id',
-            'attributes' => [
-                'value' => $resourceId,
-                'required' => true,
-            ],
-        ]);
+        $this
+            ->add([
+                'type' => Element\Hidden::class,
+                'name' => 'resource_id',
+                'attributes' => [
+                    'value' => $resourceId,
+                    'required' => true,
+                ],
+            ])
+            ->add([
+                'type' => Element\Hidden::class,
+                'name' => 'comment_parent_id',
+                'attributes' => [
+                    'id' => 'comment_parent_id',
+                    'value' => null,
+                ],
+            ])
+            ->add([
+                'type' => Element\Hidden::class,
+                'name' => 'path',
+                'attributes' => [
+                    'value' => $this->getOption('path'),
+                    'required' => true,
+                ],
+            ])
+            ->add([
+                'type' => Element\Csrf::class,
+                'name' => sprintf('csrf_%s', $resourceId),
+                'options' => [
+                    'csrf_options' => ['timeout' => 3600],
+                ],
+            ])
+            ->add([
+                'type' => Element\Submit::class,
+                'name' => 'submit',
+                'attributes' => [
+                    'class' => 'fas fa-comment',
+                    'value' => 'Comment it!', // @translate
+                ],
+            ]);
 
-        $this->add([
-            'type' => Hidden::class,
-            'name' => 'comment_parent_id',
-            'attributes' => [
-                'id' => 'comment_parent_id',
-                'value' => null,
-            ],
-        ]);
-
-        $this->add([
-            'type' => Hidden::class,
-            'name' => 'path',
-            'attributes' => [
-                'value' => $this->getOption('path'),
-                'required' => true,
-            ],
-        ]);
-
-        $this->add([
-            'type' => Csrf::class,
-            'name' => sprintf('csrf_%s', $resourceId),
-            'options' => [
-                'csrf_options' => ['timeout' => 3600],
-            ],
-        ]);
-
-        $this->add([
-            'type' => Button::class,
-            'name' => 'submit',
-            'options' => [
-                'label' => 'Comment it!', // @translate
-            ],
-            'attributes' => [
-                'class' => 'fa fa-comment',
-            ],
-        ]);
-
-        $inputFilter = $this->getInputFilter();
-        $inputFilter->add([
-            'name' => 'o-module-comment:website',
-            'required' => false,
-        ]);
+        $this->getInputFilter()
+            ->add([
+                'name' => 'o-module-comment:website',
+                'required' => false,
+            ]);
     }
 
     /**
