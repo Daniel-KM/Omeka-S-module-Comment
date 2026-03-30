49881e16a2899611b69700517a99e77f6bb68221
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-03-30 00:00:00 +0000

    Fixed deprecated null argument in signature.

--- src/Service/ViewHelper/CommentsSearchFormFactory.php
+++ src/Service/ViewHelper/CommentsSearchFormFactory.php
@@ -8,7 +8,7 @@ use Laminas\ServiceManager\Factory\FactoryInterface;
 
 class CommentsSearchFormFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
+    public function __invoke(ContainerInterface $services, $requestedName, ?array $options = null)
     {
         $formElementManager = $services->get('FormElementManager');
 
--- src/Site/BlockLayout/CommentsBrowse.php
+++ src/Site/BlockLayout/CommentsBrowse.php
@@ -46,8 +46,8 @@ class CommentsBrowse extends AbstractBlockLayout implements TemplateableBlockLay
     public function form(
         PhpRenderer $view,
         SiteRepresentation $site,
-        SitePageRepresentation $page = null,
-        SitePageBlockRepresentation $block = null
+        ?SitePageRepresentation $page = null,
+        ?SitePageBlockRepresentation $block = null
     ) {
         // Factory is not used to make rendering simpler.
         $services = $site->getServiceLocator();
